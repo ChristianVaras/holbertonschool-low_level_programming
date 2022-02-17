644a5c7c0471eee10539a2af8c7236268185f9c9
ChristianVaras <christianvaras.2020@gamil.com>
2022-02-16 20:54:22 -0800

    Add header file and function's files for task 0 and 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+        int i = 0;
+        int j = 0;
+
+        while (dest[i] != '\0')
+                i++;
+
+        while (src[j] != src[n])
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+        dest[i] = '\0';
+
+        return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# 0x06 - More pointers,arrays & strings.
+In this project, I continued to practice using pointers, arrays and strings in C.
