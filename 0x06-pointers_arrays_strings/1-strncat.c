8c0a51dd5bc3d1bae905dedc5595d899f898ac51
ChristianVaras <christianvaras.2020@gamil.com>
2022-02-16 21:31:57 -0800

    Fixing code for task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,29 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings
+ * _strncat - concatenate n bytes of src to dest string
  * @dest: string to be appended to
  * @src: string to append
+ * @n: append n numbers of bytes(chars)
  * Return: concatenated string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int i = 0;
-        int j = 0;
+	int i = 0;
+	int j = 0;
 
-        while (dest[i] != '\0')
-                i++;
+	while (dest[i] != '\0')
+		i++;
 
-        while (src[j] != src[n])
-        {
-                dest[i] = src[j];
-                i++;
-                j++;
-        }
-        dest[i] = '\0';
+	while (src[j] != src[n])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-        return (dest);
+	dest[i] = '\0';
+
+	return (dest);
 }
