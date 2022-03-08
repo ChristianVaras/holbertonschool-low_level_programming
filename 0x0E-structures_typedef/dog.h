a1b2828a517480ed26b3ce67cfcbb4ac55651533
ChristianVaras <christianvaras.2020@gamil.com>
2022-03-07 20:35:14 -0800

    Define a new type struct dog for task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - structure for dogs
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+#endif
