6c3d68686618a23b3ef859d81792e83150deb09c
ChristianVaras <christianvaras.2020@gmail.com>
2022-05-27 12:28:22 -0500

    Fix code style

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,9 +10,10 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int idx;
+
 	if (size == 0)
-		return(0);
+		return (0);
 
 	idx = hash_djb2(key);
-	return(idx % size);
+	return (idx % size);
 }
